01dec80790d69f8e1eec7772e24be6e43eba6669
harshit <harshitshukla36@gmail.com>
2023-03-30 21:46:19 +0530

    day 4 outside

---
+++ day4/sqlalchemy_demo/app.py
@@ -0,0 +1,44 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base,Employee
+
+##create a database
+
+db_name = "records.db"
+
+#### how to connect to different kinds of databases?
+
+## a) SQLITE
+
+url = f"sqlite:///{db_name}"
+
+#engine : a variable that stores connection details
+engine = create_engine(url)
+
+#### Base knows how many models are to be created
+#all tables will be created automatically now
+Base.metadata.create_all(engine)
+
+#since engine has credentials for connection stored, initiate a session
+Session_Class = sessionmaker(bind=engine) #Session class
+
+#session variable: object of Session
+session = Session_Class()
+
+e1 = Employee(eid=101,name="Harshit", age=32, location="Mumbai", salary=40000)
+e2 = Employee(eid=202,name="Shreyanshi",age = 31, location="Bengaluru", salary=50000)
+
+#notice how add_all performed (INSERT INTO TABLE NAME QUERY automatically)
+session.add_all([e1,e2])
+session.commit()
+
+
+"""
+
+step 1: user interacts with front-end UI(web-page or app screen)
+step 2: we capture data from the user and send it to the API layer
+    (user-data--->JSON---->python_object)[   job of API   ]
+step 3: PYTHON data is translated by SQL Alchemy into SQL rows
+        BASED ON SOME REFERENCE SCHEMA (implemented as a class in python)
+step 4: data reaches SQL database table as a SQL row
+"""
---
+++ day4/sqlalchemy_demo/models.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column,Integer,String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Employee(Base):
+
+    __tablename__ = "Employee"  
+
+    employee_id = Column("employee_id", Integer, primary_key=True)
+    employee_name = Column("employee_name", String)
+    employee_age = Column("employee_age", Integer)
+    employee_location = Column("employee_location", String)
+    employee_salary=Column("employee_salary", Integer)
+
+    def __init__(self,eid,name, age, location, salary) -> None:
+        self.employee_id=eid
+        self.employee_name = name
+        self.employee_age = age
+        self.employee_location = location
+        self.employee_salary=salary
+
+    def __repr__(self) -> str:
+        return f"{self.__dict__}"
